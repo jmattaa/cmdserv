ab600d03233d3556a8176393ba05109aa598f6a8
jmattaa <mattajonathan1@gmail.com>
2025-08-03 20:49:50 +0200

    idk where i want to go with ts

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jmattaa/cmdserv
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/jmattaa/cmdserv/middleware"
+)
+
+const DEBUG = true
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := exec.Command("open", "/Applications/Spotify.app").Run()
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err)
+		}
+	})
+
+	var h http.Handler = mux
+	if DEBUG {
+		h = middleware.Logger(h)
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", h))
+}
